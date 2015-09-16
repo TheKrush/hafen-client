TheKrush/hafen-client-minion-old

observe floating map option to properly update checkbox in options

src/haven/GameUI.java
@@ -609,6 +609,7 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	} else if(w == mmappanel){
 	    mmappanel = null;
 	    placemmap();
+	    CFG.MMAP_FLOAT.set(false, true);
 	}
 	meters.remove(w);
     }

src/haven/OptWnd.java
@@ -303,14 +303,34 @@ public class OptWnd extends Window {
 	display.add(new CFGBox("Store chat logs", CFG.STORE_CHAT_LOGS, "Logs are stored in 'chats' folder"), new Coord(x, y));
 
 	y += 25;
-	display.add(new CFGBox("Undock minimap", CFG.MMAP_FLOAT){
+	display.add(new CFGBox("Undock minimap", CFG.MMAP_FLOAT) {
+	    {
+		CFG.MMAP_FLOAT.setObserver(new CFG.Observer() {
+		    @Override
+		    public void updated(CFG cfg) {
+			update(cfg);
+		    }
+		});
+	    }
+
 	    @Override
 	    public void set(boolean a) {
 		super.set(a);
-		if(ui != null && ui.gui != null){
+		if(ui != null && ui.gui != null) {
 		    ui.gui.showmmappanel(a);
 		}
 	    }
+
+	    @Override
+	    public void destroy() {
+		CFG.MMAP_FLOAT.setObserver(null);
+		super.destroy();
+	    }
+
+	    private void update(CFG cfg) {
+		a = cfg.valb();
+	    }
+
 	}, new Coord(x, y));
 
 	my = Math.max(my, y);

TheKrush/hafen-client

src/haven/GameUI.java
@@ -670,6 +670,7 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 		} else if (w == mmappanel) {
 			mmappanel = null;
 			placemmap();
+			CFG.UI_MINIMAP_FLOATING.set(false, true);
 		}
 		meters.remove(w);
 	}

src/haven/OptWnd.java
@@ -597,6 +597,14 @@ public class OptWnd extends Window {
 		panel.add(new CFGCheckBox("Store chat logs", CFG.UI_CHAT_LOGS, "Logs are stored in 'chats' folder"), new Coord(x, y));
 		y += 25;
 		panel.add(new CFGCheckBox("Undock minimap", CFG.UI_MINIMAP_FLOATING) {
+			{
+				CFG.UI_MINIMAP_FLOATING.setObserver(new CFG.Observer() {
+					@Override
+					public void updated(CFG cfg) {
+						update(cfg);
+					}
+				});
+			}
 
 			@Override
 			public void set(boolean a) {
@@ -604,6 +612,16 @@ public class OptWnd extends Window {
 				if (ui != null && ui.gui != null) {
 					ui.gui.showmmappanel(a);
 				}
+			}
+
+			@Override
+			public void destroy() {
+				CFG.UI_MINIMAP_FLOATING.setObserver(null);
+				super.destroy();
+			}
+
+			private void update(CFG cfg) {
+				a = cfg.valb();
 			}
 		}, new Coord(x, y));
 		y += 25;