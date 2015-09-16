TheKrush/hafen-client-minion-old

added option to save chat logs to disk

src/haven/OptWnd.java
@@ -296,6 +296,12 @@ public class OptWnd extends Window {
 	y += 25;
 	display.add(new CFGBox("Show flavor objects", CFG.DISPLAY_FLAVOR), new Coord(x, y));
 
+	y += 25;
+	display.add(new CFGBox("Show timestamps in chat objects", CFG.SHOW_CHAT_TIMESTAMP), new Coord(x, y));
+
+	y += 25;
+	display.add(new CFGBox("Store chat logs", CFG.STORE_CHAT_LOGS, "Logs are stored in 'chats' folder"), new Coord(x, y));
+
 	x += 250;
 	y = 0;
 	my = Math.max(my, y);

TheKrush/hafen-client

src/haven/OptWnd.java
@@ -525,6 +525,8 @@ public class OptWnd extends Window {
 
 		panelUI.add(new CFGCheckBox("Show timestamps in chat", CFG.UI_CHAT_TIMESTAMP), new Coord(x, y));
 		y += 25;
+		panelUI.add(new CFGCheckBox("Store chat logs", CFG.UI_CHAT_LOGS, "Logs are stored in 'chats' folder"), new Coord(x, y));
+		y += 25;
 		panelUI.add(new CFGCheckBox("Item meter countdown", CFG.UI_ITEM_METER_COUNTDOWN, "If checked all item progress meters will start full and empty over time."), new Coord(x, y));
 		y += 25;
 		panelUI.add(new Label("Item meter"), new Coord(x, y));