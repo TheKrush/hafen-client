d3619017886f3de3ed32db718c21f8e1a3cffcc9
TheKrush <krush@lostminions.org>
2015-09-15 22:47:44 -0700

    removing some files

--- etc/run-debug.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-java -Xms512m -Xmx1024m -jar hafen.jar -U http://game.havenandhearth.com/hres/ game.havenandhearth.com
-
-pause
--- etc/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-start javaw -Xms512m -Xmx1024m -jar hafen.jar -U http://game.havenandhearth.com/hres/ game.havenandhearth.com
