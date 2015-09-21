430560ba05823d3dbc4e5d5898760f2b3ceab697
TheKrush <krush@lostminions.org>
2015-09-21 15:29:13 -0700

    minor fix for quality single function

--- src/haven/QualityList.java
+++ src/haven/QualityList.java
@@ -62,11 +62,11 @@ public class QualityList {
 	}
 
 	public Quality single() {
-		return single(false);
+		return single(CFG.UI_ITEM_QUALITY_SHOW.vali() == 2);
 	}
 
 	public Quality single(boolean isAverage) {
-		return CFG.UI_ITEM_QUALITY_SHOW.vali() == 2 ? max : average;
+		return isAverage ? max : average;
 	}
 
 	public Quality single(QualityType qualityType) {
